d439a9edd14a9c6772b068554ba5ccc3341707bc
Dastonbek <yakubovdastonbek@gmail.com>
2023-01-31 22:46:40 +0500

    I completed all of the tasks in 20 days. lol

--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
+        int uyxla=0;
+        while (uyxla<=lastPrinted){
+            System.out.println(uyxla);
+            uyxla++;
+        }
     }
 }
